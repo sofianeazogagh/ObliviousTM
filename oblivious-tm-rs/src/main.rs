0923af3d248c77213801ccda176c3e5f9cc69be1
birbaubin <aubin.birba@gmail.com>
2023-02-27 17:12:38 -0500

    Blind rotation test added

--- oblivious-tm-rs/src/main.rs
+++ oblivious-tm-rs/src/main.rs
@@ -1,17 +1,22 @@
 
-mod blind_array_access_2d;
+// mod blind_array_access_2d;
+mod blind_rotation;
+// mod helpers;
 // mod pbs_example;
 
 // use crate::test_baacc2d::test_baacc2d;
 
-use crate::blind_array_access_2d::blind_array_access2d;
+// use crate::blind_array_access_2d::blind_array_access2d;
+use crate::blind_rotation::test_blind_rotation;
 
 
 
 
 pub fn main() {
 
-    blind_array_access2d();
+    // blind_array_access2d();
+
+    test_blind_rotation();
 
 
 
