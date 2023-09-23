6988e9aa66605549ad5b14dd5f3bc01a947f9585
sofianeazogagh <sofiane.a.f@hotmail.fr>
2023-09-23 12:27:06 -0400

    PoC-Otm Inverse and x2

--- oblivious-tm-rs/src/oblivious_tm.rs
+++ oblivious-tm-rs/src/oblivious_tm.rs
@@ -1,3 +1,6 @@
+
+const DEBUG: bool = false;
+
 use revolut::*;
 use tfhe::shortint::parameters::*;
 use tfhe::core_crypto::prelude::*;
@@ -8,8 +11,8 @@ pub fn oblivious_tm()
 {
     //The number of steps our Turing Machine will run.
 
-    let step = 14;
-    let param = PARAM_MESSAGE_3_CARRY_0;
+    let step = 7;
+    let param = PARAM_MESSAGE_4_CARRY_0;
     let mut ctx = Context::from(param);
     let private_key = PrivateKey::new(&mut ctx);
     let public_key = private_key.get_public_key();
@@ -17,7 +20,7 @@ pub fn oblivious_tm()
     println!("Key generated");
 
     //creation of tape
-    let mut tape = vec![1,0,0,1];
+    let mut tape = vec![0,0,0,0];
     while tape.len() < ctx.message_modulus().0 {
         tape.push(2_u64);
     }
@@ -35,12 +38,51 @@ pub fn oblivious_tm()
 
     // POUR FAIRE QUE LA BANDE TOURNE À GAUCHE IL FAUT METTRE 2*FULL_MOD - ROTATE_LEFT ? donc 31 dans le cas 4_0  
 
-    //// inverser 0 et 1 sans retour de la tête de lecture
-    let instruction_write = vec![
-        vec![1,1,0], // pourquoi qd je lis 1 je fais 1 + 1 pour avoir 0 ? parce que je lis 15 (-1) et pas 1 donc il faut faire 15 + 1 = 16 = 0
+
+    // //// Multiplication par 2
+    // let instruction_write = vec![
+    //     vec![0,0,2], // pourquoi qd je lis 1 je fais 1 + 1 pour avoir 0 ? parce que je lis 15 (-1) et pas 1 donc il faut faire 15 + 1 = 16 = 0
+    //     vec![0,0,0]
+
+    // ];
+    // let instruction_position = vec![ // mouvement de la tête à gauche = mouvement de la bande à droite = rotation de 31
+    //     vec![1,1,0], // mouvement de la tête à droite = mouvement de la bande à gauche = rotation de 1
+    //     vec![0,0,0]
+    // ];
+    // let instruction_state = vec![
+    //     vec![0,0,1],
+    //     vec![1,1,1]
+    // ];
+
+
+
+
+
+    // //// soustraire 1 
+    // let instruction_write = vec![
+    //     vec![0,0,0], // pourquoi qd je lis 1 je fais 1 + 1 pour avoir 0 ? parce que je lis 15 (-1) et pas 1 donc il faut faire 15 + 1 = 16 = 0
+    //     vec![15,1,0],
+    //     vec![0,0,0]
+
+    // ];
+    // let instruction_position = vec![ // mouvement de la tête à gauche = mouvement de la bande à droite = rotation de 31
+    //     vec![1,1,31], // mouvement de la tête à droite = mouvement de la bande à gauche = rotation de 1
+    //     vec![31,31,31],
+    //     vec![0,0,0]
+    // ];
+    // let instruction_state = vec![
+    //     vec![0,0,1],
+    //     vec![1,2,2],
+    //     vec![2,2,2]
+    // ];
+
+
+
+    //// inverser 0 et 1 
+    let instruction_write = vec![ // pourquoi qd je lis 0 je fais 0 + 15 pour avoir 0 ? parce que je lis 0 et je veux ecrire 1  donc 0 + 15 = -1 = 1
+        vec![15,1,0], // qd je lis 1 je fais 1 + 1 pour avoir 0, parce que je lis 15 (=-1) et pas 1 donc il faut faire 15 + 1 = 16 = 0
         vec![0,0,0],
         vec![0,0,0]
-
     ];
     let instruction_position = vec![
         vec![1,1,0],
@@ -54,58 +96,38 @@ pub fn oblivious_tm()
     ];
 
 
-
-    //// inverser 0 et 1 avec retour de la tête de lecture (sensible et long avec 4_0)
-    // let instruction_write = vec![
-    //     vec![1,1,0],
-    //     vec![0,0,0],
-    //     vec![0,0,0]
-
-    // ];
-    // // complete_matrix(&mut instruction_write, ctx.message_modulus().0, ctx.message_modulus().0);
-    
-    // let mut instruction_position = vec![
-    //     vec![1,1,31],
-    //     vec![31,31,0],
-    //     vec![0,0,1]
-    // ];
-    // // complete_matrix(&mut instruction_position, ctx.message_modulus().0, ctx.message_modulus().0);
-    // // instruction_position[1][ctx.message_modulus().0 - 2] = 31; // 1 dans la partie negacyclic
-
-
-    // let mut instruction_state = vec![
-    //     vec![0,0,1],
-    //     vec![1,1,2],
-    //     vec![2,2,2]
-    // ];
-    // complete_matrix(&mut instruction_state, ctx.message_modulus().0, ctx.message_modulus().0);
-    // instruction_state[1][ctx.message_modulus().0 - 2] = 30; // 2 dans la partie negacyclic
-
-
-
     let ct_instruction_write = private_key.encrypt_matrix(&mut ctx, &instruction_write);
     let ct_instruction_position = private_key.encrypt_matrix(&mut ctx, &instruction_position);
     let ct_instruction_state = private_key.encrypt_matrix(&mut ctx, &instruction_state);
 
     println!("Instructions Encrypted");
 
+    let mut nb_of_move = public_key.allocate_and_trivially_encrypt_lwe(0, &ctx);
+
     println!("Oblivious TM Start..");
     for i in 0..step {
 
         println!("--- STEP {} ",i);
 
-        let cell_content = read_cell_content(&tape, &public_key, &ctx,&private_key);
+        let cell_content = read_cell_content(&tape, &public_key, &ctx);
+
+        if DEBUG {
         private_key.debug_lwe("State ", &state, &ctx); //line
         private_key.debug_lwe("Cell content", &cell_content, &ctx); //column
+        }
 
-
-        write_new_cell_content(&mut tape, &cell_content, &state, &ct_instruction_write, public_key, &ctx,&private_key); // ecris 0 - 7 - 0 - 0
-        change_head_position(&mut tape, &cell_content, &state, &ct_instruction_position, public_key, &ctx,&private_key); // rotate de 1 - 7 - 0 - 0
-        state = get_new_state(&cell_content, &state, &ct_instruction_state, public_key, &ctx,&private_key); // 1 - 3 - 0 - 6 -
+        write_new_cell_content(&mut tape, &cell_content, &state, &ct_instruction_write, public_key, &ctx,&private_key);
+        change_head_position(&mut tape, &cell_content, &state, &ct_instruction_position, public_key, &ctx, &mut nb_of_move, &private_key); 
+        state = get_new_state(&cell_content, &state, &ct_instruction_state, public_key, &ctx,&private_key);
         print!("New Tape : ");
         tape.print(&private_key, &ctx);
 
     }
+
+    println!("Oblivious TM End... \nReordering the tape..");
+    public_key.wrapping_neg_lwe(&mut nb_of_move);
+    blind_rotate_assign(&nb_of_move, &mut tape.0, &public_key.fourier_bsk);
+
 
 
     println!("---------------  FINAL TAPE ---------------");
@@ -125,19 +147,11 @@ pub fn read_cell_content(
     tape: &LUT,
     public_key: &PublicKey,
     ctx: &Context,
-    private_key : &PrivateKey
-
 ) -> LweCiphertext<Vec<u64>> 
 {
-    // let mut cell_content = LweCiphertext::new(0u64, ctx.big_lwe_dimension().to_lwe_size(), ctx.ciphertext_modulus());
-    // extract_lwe_sample_from_glwe_ciphertext(&tape.0, &mut cell_content, MonomialDegree(0));
-    // let mut switched = LweCiphertext::new(0_64, ctx.small_lwe_dimension().to_lwe_size(), ctx.ciphertext_modulus());
-    // keyswitch_lwe_ciphertext(&public_key.lwe_ksk, &cell_content, &mut switched);
-
     let mut ct_0 = LweCiphertext::new(0, ctx.small_lwe_dimension().to_lwe_size(), ctx.ciphertext_modulus());
     trivially_encrypt_lwe_ciphertext(&mut ct_0, Plaintext(ctx.full_message_modulus() as u64));
     let cell_content = public_key.blind_array_access(&ct_0, &tape, &ctx);
-    // private_key.debug_lwe("cell_content = ", &cell_content, ctx);
 
     return cell_content;
 }
@@ -157,19 +171,11 @@ pub fn write_new_cell_content(
 
 
     let new_cell_content = public_key.blind_matrix_access(&ct_instruction_write, &state, &cell_content, &ctx);
-
     let lut_new_cell_content = LUT::from_lwe(&new_cell_content,&public_key,&ctx);
+    if DEBUG{
     private_key.debug_lwe("(W) new cell content = ", &new_cell_content, ctx);
-
-    // print!("lut new cell content ");
-    // lut_new_cell_content.print(&private_key, &ctx);
-
-    // println!("BEFORE GLWE SUM");
-
-    // private_key.debug_glwe("tape = ", &tape.0, &ctx);
-    // private_key.debug_glwe("lut new cell ", &lut_new_cell_content.0, &ctx);
+    }
     public_key.glwe_sum_assign(&mut tape.0, &lut_new_cell_content.0);
-    // private_key.debug_glwe("AFTER GLWE SUM \n", &tape.0, &ctx);
 }
 
 
@@ -181,12 +187,16 @@ pub fn change_head_position(
     ct_instruction_position: &Vec<LUT>,
     public_key: &PublicKey,
     ctx: &Context,
+    nb_of_move : &mut LweCiphertext<Vec<u64>>,
     private_key : &PrivateKey
 )
 {
 
     let position_change = public_key.blind_matrix_access(&ct_instruction_position,&state , &cell_content, &ctx);
+    if DEBUG {
     private_key.debug_lwe("(P) next move = ", &position_change, ctx);
+    }
+    lwe_ciphertext_add_assign(nb_of_move, &position_change);
     blind_rotate_assign(&position_change, &mut tape.0, &public_key.fourier_bsk);
 
 }
@@ -202,22 +212,10 @@ pub fn get_new_state(
 {
 
     let new_state = public_key.blind_matrix_access(&ct_instruction_state, &state, &cell_content, &ctx);
+    if DEBUG{
     private_key.debug_lwe("(S) new state = ", &new_state, ctx);
+    }
 
     return new_state
 }
 
-
-fn complete_matrix(matrix: &mut Vec<Vec<u64>>, target_rows: usize, target_cols: usize) {
-    // Vérifier si la matrice doit être agrandie en ajoutant des zéros en bas
-    while matrix.len() < target_rows {
-        matrix.push(vec![0; matrix[0].len()]);
-    }
-
-    // Vérifier si la matrice doit être agrandie en ajoutant des zéros à droite
-    for row in matrix.iter_mut() {
-        while row.len() < target_cols {
-            row.push(0);
-        }
-    }
-}
